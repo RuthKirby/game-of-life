e957f7623da66f850f30677886ad40b9d4effe4a
ruthbovell <ruthb111@outlook.com>
2019-02-07 17:02:03 +0000

    Generated javadocs.

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"game","l":"Cell"},{"p":"game","l":"Game"},{"p":"game","l":"Grid"},{"p":"game","l":"Launcher"},{"p":"game","l":"Tile"},{"p":"game","l":"Grid.TileScenario"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"game","l":"Cell"},{"p":"game","l":"Game"},{"p":"game","l":"Grid"},{"p":"game","l":"Launcher"},{"p":"game","l":"Tile"},{"p":"game","l":"TileScenario"}]
